ae81d2e5e815c7fd726f648b7442356e8d98d21f
Keith Daulton <kdaulton@gmail.com>
2019-04-19 01:26:08 -0400

    Fixes charts not updating on prop changes

--- app/components/stock-chart.js
+++ app/components/stock-chart.js
@@ -1,18 +1,39 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { observer } from '@ember/object';
+import { once } from '@ember/runloop';
 
 export default Component.extend({
     chartBuilder: service(),
     stock: null,
     data: null,
-    didInsertElement() {
-        this._super(...arguments);
-
+    chartInstance: null,
+    updateChart() {
         let data = this.get('data');
-        if (!data || !data.length) {
+        let stock = this.get('stock');
+        const chartInstance = this.get('chartInstance');
+
+        if (!stock || !data || !data.length) {
+            if (chartInstance) {
+                chartInstance.destroy();
+                this.set('chartInstance', null);
+            }
             return;
         }
 
-        this.chartBuilder.createLineChart(this.element.querySelector('.o-chart'), this.data, this.stock.symbol);
-    }
+        if (chartInstance) {
+            const series = chartInstance.series[0];
+            series.update({ name: stock.symbol }, false);
+            series.setData(data);
+        } else {
+            this.set('chartInstance', this.chartBuilder.createLineChart(this.element.querySelector('.o-chart'), data, stock.symbol));
+        }
+    },
+    didInsertElement() {
+        this._super(...arguments);
+        this.updateChart();
+    },
+    attrsChanged: observer('stock', 'data', function() {
+        once(this, 'updateChart');
+    })
 });
